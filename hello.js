d31b2eb9694f36c3b8351755b08fa9e0d6ed6207
mdalamin.eu <mdalamin.eu@gmail.com>
2019-12-08 21:54:05 +0200

    remove configure object

--- hello.js
+++ hello.js
@@ -4,6 +4,5 @@ Object.defineProperty(typeof global === "object" ? global : window, "PI", {
     value:        3.141593,
     enumerable:   true,
     writable:     false,
-    configurable: false
 })
 PI > 3.0;
